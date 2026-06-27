fe148a90131d8205cba7c22e3d98dca3b32280e2
Edward Walkerdine <edwardwalkerdine77@gmail.com>
2026-06-27 20:39:15 +0100

    fix supabase url

--- supabase_client.py
+++ supabase_client.py
@@ -1,8 +1,8 @@
 from supabase import create_client, Client
 
 # ---------------- SUPABASE CONNECTION ----------------
-SUPABASE_URL = "PASTE_YOUR_URL_HERE"
-SUPABASE_KEY = "PASTE_YOUR_ANON_KEY_HERE"
+SUPABASE_URL = "https://dsjhvymuaaoqcgavidrb.supabase.co"
+SUPABASE_KEY = "sb_publishable_h1tLW3yVRLXVyu5aDYb3YQ_H0IehfAY"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -12,7 +12,7 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 def get_user(username: str):
     """Fetch a user from Supabase"""
     response = supabase.table("users").select("*").eq("username", username).execute()
-    
+
     if response.data:
         return response.data[0]
     return None
